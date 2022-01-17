22cc6d4cae8112d77588d9bfe02225d79b5b2935
NoshGit <noshirpatel86@gmail.com>
2022-01-17 21:16:50 +0530

    Basic Changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Practice Project</h1>`;
 
 /**
  * Place or replace code below from respective JS files to check functionalities
